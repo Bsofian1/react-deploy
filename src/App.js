9abe1ac96f500fc134248296c664b48980f2fc1a
Sofian <74557858+Bsofian1@users.noreply.github.com>
2021-06-24 13:06:34 +0200

    deploy my react app to github

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My react app</h1>
+      <p>a simple react deploy</p>
+    </div>
+  );
+}
+
+export default App;
